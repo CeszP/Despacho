8f3bb2029b1b6b894f1d66af7310ee6100316f62
CeszP <penalozacesz@gmail.com>
2017-08-10 00:50:00 -0500

    Agregando un update
    Agregando comentario para poder hacer el primer pull sobre mi repositorio local.. :D

--- src/backend/ControlBD.java
+++ src/backend/ControlBD.java
@@ -14,7 +14,7 @@ import java.sql.Statement;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
-
+//Hola!!
 public class ControlBD {
     /*::::DEFINICIÓN DE VARIABLES::::*/
     /*VARIABLES MySQL*/
